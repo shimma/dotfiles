b50d0c2d32c4ee50435eae23bc0dab1a6b16498e
shimma <shimma.dev@gmail.com>
2018-05-06 18:49:17 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,63 +32,59 @@ brew install caskroom/cask/brew-cask      || true
 brew install coreutils                    || true
 brew install ctags                        || true
 brew install curl                         || true
+brew install diff-so-fancy                || true
 brew install findutils                    || true
 brew install git                          || true
 brew install go                           || true
 brew install htop                         || true
 brew install hub                          || true
+brew install imagemagick                  || true
 brew install jq                           || true
 brew install mcrypt                       || true
+brew install mysql                        || true
 brew install neovim                       || true
 brew install nkf                          || true
 brew install node                         || true
 brew install openssl                      || true
 brew install peco                         || true
+brew install pwgen                        || true
 brew install python3                      || true
 brew install rbenv                        || true
-brew install ruby-build                   || true
 brew install reattach-to-user-namespace   || true
-ln -s $(which reattach-to-user-namespace) ~/bin/i
+brew install ruby-build                   || true
 brew install tig                          || true
 brew install tmux                         || true
 brew install vim                          || true
 brew install wget                         || true
 brew install xz                           || true
-brew install zsh                          || true
-brew install imagemagick                  || true
-brew install mysql                        || true
-#brew install git-flow-avh                 || true
-#brew install composer                     || true
-brew install diff-so-fancy                || true
-brew install pwgen                        || true
 brew install yarn                         || true
+brew install zsh                          || true
+ln -s $(which reattach-to-user-namespace) ~/bin/i
 
-
-brew cask install visual-studio-code      || true
-#brew cask install slack                   || true
-brew cask install dockertoolbox           || true
-brew cask install font-anonymous-pro      || true
 #brew cask install font-fontawesome        || true
 #brew cask install font-lobster            || true
 #brew cask install font-noto-sans-japanese || true
+#brew cask install hoster                  || true
+#brew cask install slack                   || true
+#brew cask install sourcetree              || true
+brew cask install anaconda                || true
+brew cask install cmd-eikana              || true
+brew cask install dockertoolbox           || true
+brew cask install font-anonymous-pro      || true
 brew cask install font-ricty-diminished   || true
 brew cask install google-chrome           || true
-brew cask install google-drive            || true
 brew cask install google-japanese-ime     || true
-#brew cask install hoster                  || true
 brew cask install iterm2                  || true
 brew cask install licecap                 || true
 brew cask install mysqlworkbench          || true
+brew cask install ngrok                   || true
+brew cask install sequel-pro              || true
 brew cask install skype                   || true
-#brew cask install sourcetree              || true
 brew cask install spectacle               || true
 brew cask install the-unarchiver          || true
 brew cask install vagrant                 || true
 brew cask install virtualbox              || true
-brew cask install cmd-eikana              || true
-brew cask install anaconda                || true
-brew cask install sequel-pro              || true
-brew cask install ngrok                   || true
+brew cask install visual-studio-code      || true
 brew update                               || true
 brew cleanup                              || true
 brew cask cleanup --outdated
@@ -114,10 +110,9 @@ sudo easy_install pip
 pip3 install neovim
 vim -c "PlugInstall" -c ":q" -c ":q"
 go get github.com/motemen/ghq
-#curl -s api.sdkman.io | bash
 
 brew install homebrew/versions/v8-315
 bundle config build.libv8 --with-system-v8
 bundle config build.therubyracer --with-v8-dir=/usr/local/opt/v8-315/
-curl get.pow.cx | s ca
+curl get.pow.cx | sh
 curl https://sdk.cloud.google.com | bash
