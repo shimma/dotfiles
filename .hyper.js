3c1245cb6f254cf6a7f57ced37150733f20944ed
shimma <shimma.dev@gmail.com>
2018-05-21 09:32:31 +0900

    update

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyper-ayu'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
