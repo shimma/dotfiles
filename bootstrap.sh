2cc70962d92f9baaa99fd9e9954b48545a4b16fc
shimma <shimma.dev@gmail.com>
2018-08-30 18:50:32 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -107,3 +107,5 @@ curl https://sdk.cloud.google.com | bash
 
 
 #gcloud components install kubectl
+
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
